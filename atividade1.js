da6f8debf80a8f6f3635bcf1ace48e61a099dcdb
Erickbellot12 <111981561+Erickbellot12@users.noreply.github.com>
2022-09-28 10:14:51 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,5 @@
+/*1- é uma linguagem que atravez de uma serie de instruções, transmite ordens que iram caracterizar o funcionamento de uma maquina.
+2 -exerce controle sobre o html e css paramanipular os comportamentos da web.
+3-navegador web
+4- é popular porque permitiu a escabilidade necessaria na computação e no back end
+5-porque para aprender precisamos estar sempre estudando e se atualizando então por isso é bom ter persistencia. e a tenção aos detalhes é bom porque basta um pequeno erro para um codigo gigante não funcionar.*/
